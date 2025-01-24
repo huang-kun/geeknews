71460f7b65040e1133031d25c6c56e7b2202c116
huang-kun <jack-huang-developer@foxmail.com>
2025-01-24 21:42:16 +0800

    feat: add wechat public platform config.

--- geeknews/config.py
+++ geeknews/config.py
@@ -14,3 +14,15 @@ class GeeknewsEmailConfig:
         cls.smtp_port = configparser.get_integer(cls.section, 'smtp_port')
         cls.beta_tester_path = configparser.get_abs_path(cls.section, 'beta_tester_path')
         return cls()
+    
+
+class GeeknewsWechatPPConfig:
+
+    section = 'WechatPP'
+
+    access_token_path: str
+
+    @classmethod
+    def get_from_parser(cls, configparser: GeeknewsConfigParser = GeeknewsConfigParser()):
+        cls.access_token_path = configparser.get_abs_path(cls.section, 'access_token_path')
+        return cls()
