75cb9ebcd27dd2d592053f1ba0d2ff5a084e5288
huang-kun <jack-huang-developer@foxmail.com>
2025-03-23 19:54:41 +0800

    feat: make simple web api for preview editing.

---
+++ geekapp.py
@@ -0,0 +1,57 @@
+import os
+import json
+
+from flask import Flask
+from flask import request
+
+from geeknews.manager import GeeknewsManager
+from geeknews.utils.date import GeeknewsDate
+
+app = Flask(__name__)
+geeknews_manager = GeeknewsManager()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Geeknews!</p>"
+
+@app.route("/api/update_preview")
+def update_preview():
+    format, locale, date = get_preview_params()
+    preview_path = geeknews_manager.hackernews_manager.get_preview(date, locale)
+    return load_preview_data(preview_path, format)
+
+@app.route("/api/check_preview")
+def check_preview():
+    format, locale, date = get_preview_params()
+    summary_dir = geeknews_manager.hackernews_manager.datapath_manager.get_summary_full_dir(locale, date)
+    preview_path = os.path.join(summary_dir, 'preview.md')
+    return load_preview_data(preview_path, format)
+
+@app.route("/api/set_stories_priority", methods=['POST'])
+def set_stories_priority():
+    if request.method == 'POST':
+        rule_line = request.form['rule_line']
+        date = GeeknewsDate.now().get_preview_date()
+        rule_path = geeknews_manager.hackernews_manager.api_client.make_priority_rule(rule_line, date)
+        if os.path.exists(rule_path):
+            with open(rule_path) as f:
+                rule_content = json.load(f)
+            return rule_content
+    return {}
+
+def get_preview_params():
+    format = request.args.get('format', '')
+    locale = 'zh_cn'
+    date = GeeknewsDate.now().get_preview_date()
+    return format, locale, date
+
+def load_preview_data(preview_path, format):
+    if os.path.exists(preview_path):
+        with open(preview_path) as f:
+            preview_content = f.read()
+        if format == 'json':
+            lines = preview_content.split('\n')
+            return {"data": lines}
+        else:
+            return {"data": preview_content}
+    return {}
