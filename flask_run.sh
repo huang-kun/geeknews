7dc26d8df50bd828bf21822874759981c2ef401b
huang-kun <jack-huang-developer@foxmail.com>
2025-03-30 15:38:52 +0800

    fix: modify flask run again.

--- flask_run.sh
+++ flask_run.sh
@@ -1,5 +1,5 @@
 # run flask
-nohup flask --app geekapp run --host=0.0.0.0 --port=5000 &
+nohup flask --app geekapp run --host=0.0.0.0 --port=5000 >/dev/null 2>&1 &
 
 # check flask
 # ps aux | grep geekapp
